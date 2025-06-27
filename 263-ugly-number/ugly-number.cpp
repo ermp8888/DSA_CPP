18ff83cdd09bd622a5773f5350ec3920fe2a99a9
Mohd. Parvez <mohdparvez94@gmail.com>
2025-06-27 16:19:39 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (10.08%) - LeetSync

---
+++ 263-ugly-number/ugly-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int powerCnt(int n, int div){
+        int cnt = 0;
+        while(n%div == 0){
+            cnt += 1;
+            n = n / div;
+        }
+        return cnt;
+    }
+    bool isUgly(int n) {
+        int a_2 = 0, b_3 = 0, c_5 = 0;
+
+        if(n <= 0)
+            return false;
+        if(n == 1)
+            return true;
+        a_2 = powerCnt(n, 2);
+        b_3 = powerCnt(n, 3);
+        c_5 = powerCnt(n, 5);
+
+        return n == pow(2, a_2)*pow(3, b_3)*pow(5, c_5);
+        
+    }
+};
