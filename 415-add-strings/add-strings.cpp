7b6bf7d3122026e73ef2d60a8d4e3d39d76fd021
Mohd. Parvez <mohdparvez94@gmail.com>
2025-07-01 11:36:29 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (61.14%) - LeetSync

--- 415-add-strings/add-strings.cpp
+++ 415-add-strings/add-strings.cpp
@@ -1,55 +1,20 @@
 class Solution {
 public:
     string addStrings(string num1, string num2) {
-        int lnN1 = num1.size(), lnN2 = num2.size();
-        string outputStr = "";
+        int l1 = num1.size() - 1, l2 = num2.size() - 1;
         int carray = 0;
-        // int smallLen = lnN1 > lnN2 ? lnN2 : lnN1;
+        string sumOfNums = "";
 
-        if(lnN1 >= lnN2){
-            for(int i = 0; i < lnN2; i++){
-                int sum = (num2[lnN2 - 1 - i] - '0') + (num1[lnN1 - 1 - i] - '0') + carray;
-                carray = sum / 10;
-                outputStr += to_string(sum % 10);
-            }
-            
-            if(carray == 0){
-                for(int i = lnN1 - lnN2 - 1; i >= 0; i--){
-                    outputStr += num1[i];
-                }
-            }else{
-                for(int i = lnN1 - lnN2 - 1; i >= 0; i--){
-                    int sum = (num1[i] - '0') + carray;
-                    carray = sum / 10;
-                    outputStr += to_string(sum % 10);
-                    // if(carray == 0)
-                    //     break;
-                }
-            }
-            if(carray == 1) outputStr += to_string(carray);
-        }else{
-            for(int i = 0; i < lnN1; i++){
-                int sum = (num1[lnN1 - 1 - i] - '0') + (num2[lnN2 - 1 - i] - '0') + carray;
-                carray = sum / 10;
-                outputStr += to_string(sum % 10);
-            }
-            if(carray == 0){
-                for(int i = lnN2 - lnN1 - 1; i >= 0; i--){
-                    outputStr += num2[i];
-                }
-            }else{
-                for(int i = lnN2 - lnN1 - 1; i >= 0; i--){
-                    int sum = (num2[i] - '0') + carray;
-                    carray = sum / 10;
-                    outputStr += to_string(sum % 10);
-                    // if(carray == 0)
-                    //     break;
-                }
-            }
-            if(carray == 1) outputStr += to_string(carray);
+        while(l1 >= 0 || l2 >= 0 || carray){
+            int d1 = (l1 >= 0) ? num1[l1--] - '0' : 0;
+            int d2 = (l2 >= 0) ? num2[l2--] - '0' : 0;
+
+            int sum = d1 + d2 + carray;
+            sumOfNums.push_back(sum % 10 + '0');
+            carray = sum / 10;
         }
         
-        reverse(outputStr.begin(), outputStr.end());
-        return outputStr;
+        reverse(sumOfNums.begin(), sumOfNums.end());
+        return sumOfNums;
     }
 };
