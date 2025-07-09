b5426af01b07a854e3530940c5ea8fa836b79ce9
Mohd. Parvez <mohdparvez94@gmail.com>
2025-07-09 10:08:42 +0530

    Time: 6 ms (19.38%) | Memory: 10.7 MB (12.84%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.cpp
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -5,13 +5,13 @@ public:
         for(auto& p0 : points){
             for(auto& p1 : points){
                 for(auto& p2 : points){
-                    double tmpArea = 0.5*(p0[0]*p1[1] + p1[0]*p2[1] + p2[0]*p0[1]
+                    double tmpArea = (p0[0]*p1[1] + p1[0]*p2[1] + p2[0]*p0[1]
                     - p1[0]*p0[1] - p2[0]*p1[1] - p0[0]*p2[1]);
                     if(tmpArea > triangleArea)
                         triangleArea = tmpArea;
                 }
             }
         }
-        return triangleArea;
+        return triangleArea / 2;
     }
 };
