c3e5c29c1c9985b4d109c57c0c8015ac7af8530c
Mohd. Parvez <mohdparvez94@gmail.com>
2025-07-09 10:05:49 +0530

    Time: 4 ms (26.06%) | Memory: 10.5 MB (34.27%) - LeetSync

---
+++ 830-largest-triangle-area/largest-triangle-area.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    double largestTriangleArea(vector<vector<int>>& points) {
+        double triangleArea = 0.0f;
+        for(auto& p0 : points){
+            for(auto& p1 : points){
+                for(auto& p2 : points){
+                    double tmpArea = 0.5*(p0[0]*p1[1] + p1[0]*p2[1] + p2[0]*p0[1]
+                    - p1[0]*p0[1] - p2[0]*p1[1] - p0[0]*p2[1]);
+                    if(tmpArea > triangleArea)
+                        triangleArea = tmpArea;
+                }
+            }
+        }
+        return triangleArea;
+    }
+};
