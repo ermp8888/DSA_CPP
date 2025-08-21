2e2c679fcde1b0e84fedc5fa339516f053810b45
Mohd. Parvez <mohdparvez94@gmail.com>
2025-08-21 10:15:34 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (23.99%) - LeetSync

--- 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
+++ 958-sort-array-by-parity-ii/sort-array-by-parity-ii.cpp
@@ -1,18 +1,38 @@
 class Solution {
 public:
+   // Time - O(n),Extra Space - O(n)
+    // vector<int> sortArrayByParityII(vector<int>& nums) {
+    //     vector<int> result(nums.size(), 0);
+    //     int even_index = 0;
+    //     int odd_index = 1;
+    //     for(int element : nums){
+    //         if((element & 1) == 0){// even
+    //             result[even_index] = element;
+    //             even_index += 2;
+    //         }else{
+    //             result[odd_index] = element;
+    //             odd_index += 2;
+    //         }
+    //     }
+    //     return result;
+    // }
+
+    // Time - O(n),Extra Space - O(1)
     vector<int> sortArrayByParityII(vector<int>& nums) {
-        vector<int> result(nums.size(), 0);
+
         int even_index = 0;
         int odd_index = 1;
-        for(int element : nums){
-            if((element & 1) == 0){// even
-                result[even_index] = element;
+        while(even_index < nums.size() && odd_index < nums.size()){
+            while(even_index < nums.size() && (nums[even_index]&1) == 0){
                 even_index += 2;
-            }else{
-                result[odd_index] = element;
+            }
+            while(odd_index < nums.size() && (nums[odd_index]&1)){
                 odd_index += 2;
             }
+            if(even_index < nums.size() && odd_index < nums.size()){
+                swap(nums[even_index], nums[odd_index]);
+            }
         }
-        return result;
+        return nums;
     }
 };
